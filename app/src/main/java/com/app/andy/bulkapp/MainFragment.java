f62b93fc245a601e177a544048f9a50c526d66e8
Andy <Andy PC>
2015-10-10 22:35:15 -0700

    re factor code

--- app/src/main/java/com/app/andy/bulkapp/MainFragment.java
+++ app/src/main/java/com/app/andy/bulkapp/MainFragment.java
@@ -3,6 +3,7 @@ package com.app.andy.bulkapp;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,40 +22,43 @@ import java.util.Date;
  */
 public class MainFragment extends Fragment {
 
-	private Button submit;
-	private EditText foodNameEdit, calorieCountEdit;
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.main_fragment, container, false);
-		init(view);
-		return view;
-	}
+    private Button submit;
+    private EditText foodNameEdit, calorieCountEdit;
 
-	/**
-	 * Initilize the view
-	 * @param view
-	 */
-	public void init(View view){
-		TextView dateText = (TextView) view.findViewById(R.id.dateTextView);
-		DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-		dateText.setText(format.format(new Date()));
-		foodNameEdit = (EditText)view.findViewById(R.id.foodEditText);
-		calorieCountEdit = (EditText)view.findViewById(R.id.calEditText);
-		submit = (Button)view.findViewById(R.id.submitButton);
-		submit.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(foodNameEdit.getText().toString().equals("") ){
-					Toast.makeText(getActivity().getBaseContext(), "You didn't enter a food name!", Toast.LENGTH_SHORT).show();
-					return;
-				}
-				else if(calorieCountEdit.getText().toString().equals("")){
-					Toast.makeText(getActivity().getBaseContext(), "You didn't enter a calorie count!", Toast.LENGTH_SHORT).show();
-					return;
-				}
-				//do save action here
-			}
-		});
-	}
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false); init(view);
+        return view;
+    }
+
+    /**
+     * Initilize the view
+     *
+     * @param view
+     */
+    public void init(View view) {
+        TextView dateText = (TextView) view.findViewById(R.id.dateTextView);
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+        dateText.setText(format.format(new Date()));
+        foodNameEdit = (EditText) view.findViewById(R.id.foodEditText);
+        calorieCountEdit = (EditText) view.findViewById(R.id.calEditText);
+        submit = (Button) view.findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foodNameEdit.getText().toString().equals("")) {
+                    Toast.makeText(getActivity().getBaseContext(), "You didn't enter a food name!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (calorieCountEdit.getText().toString().equals("")) {
+                    Toast.makeText(getActivity().getBaseContext(), "You didn't enter a calorie count!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //do save action here
+                else {
+                    Log.v("saveAction:", foodNameEdit.getText().toString() + " " + calorieCountEdit.getText().toString());
+                }
+            }
+        });
+    }
 }
